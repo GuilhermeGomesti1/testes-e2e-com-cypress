75cb6733c53c5391a938c5b4d7a081597d40f78a
Guilherme Gomes <106498363+GuilhermeGomesti1@users.noreply.github.com>
2023-12-16 01:49:17 -0300

    att

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    defaultCommandTimeout: 40000,
+    baseUrl: "https://notes-serverless-app.com/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-})
+});
